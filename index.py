563702c24714ce19edc38dacf1cc2f4bec26f03a
xwp <xwp_fullstack@163.com>
2018-06-01 11:23:28 +0800

    添加了manager

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+from flask_script import Manager
 app = Flask(__name__)
-
+manager = Manager(app)
 @app.route('/')
 def index():
     return '2018/6/1 hello python'
@@ -11,5 +11,5 @@ def news():
     return '内蒙古新闻资讯,请选择浏览'
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
 
